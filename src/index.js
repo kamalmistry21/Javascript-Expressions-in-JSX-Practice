d553458ded235a15f49c27628328151b991e69fe
Kamal Mistry <31496227+kamalmistry21@users.noreply.github.com>
2022-05-25 21:33:38 +1200

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Kamal Mistry";
+const date = new Date();
+let year = date.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
